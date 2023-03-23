1055ced9d03292d3f009bc7bb871e6ecb877fcf9
chenxin <18364062019@163.com>
2023-03-23 11:42:06 +0800

    demo

--- yingxue-videos/src/main/java/com/cjx/controller/VideoController.java
+++ yingxue-videos/src/main/java/com/cjx/controller/VideoController.java
@@ -3,7 +3,6 @@ package com.cjx.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping("/video")
 public class VideoController {
